f9ecb6a785d868d831bedab2f80f6345eb50403f
jeongguk <star7949@thinkm.co.kr>
2022-07-27 13:44:23 +0900

    URLSession, talbeView prefetch

--- Brewery/Brewery/BeerListViewController.swift
+++ Brewery/Brewery/BeerListViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class BeerListViewController: UITableViewController{
     var beerList = [Beer]()
+    var dataTasks = [URLSessionTask]()
     var currentPage = 1
     
     override func viewDidLoad() {
@@ -18,41 +19,19 @@ class BeerListViewController: UITableViewController{
         title = "페캠브루어리"
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
         tableView.rowHeight = 150
-        
+        tableView.prefetchDataSource = self
         fetchBeer(of: currentPage)
-    }
-}
-
-extension BeerListViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return beerList.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else {
-            return UITableViewCell()
-        }
         
-        let beer = beerList[indexPath.row]
-        cell.configure(with: beer)
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedBeer = beerList[indexPath.row]
-        let detailViewController = BeerDetailViewController()
-        
-        detailViewController.beer = selectedBeer
-        self.show(detailViewController, sender: nil)
     }
 }
 
 private extension BeerListViewController {
     func fetchBeer(of page: Int) {
-        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)") else { return }
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)"),
+              dataTasks.firstIndex(where: { $0.originalRequest?.url == url }) == nil else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -92,5 +71,46 @@ private extension BeerListViewController {
             }
         }
         dataTask.resume()
+        dataTasks.append(dataTask)
+    }
+}
+
+extension BeerListViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return beerList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("Rows: \(indexPath.row)")
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else {
+            return UITableViewCell()
+        }
+
+        let beer = beerList[indexPath.row]
+        cell.configure(with: beer)
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedBeer = beerList[indexPath.row]
+        let detailViewController = BeerDetailViewController()
+        
+        detailViewController.beer = selectedBeer
+        self.show(detailViewController, sender: nil)
+    }
+    
+}
+
+extension BeerListViewController: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        guard currentPage != 1 else { return }
+        
+        indexPaths.forEach {
+            if($0.row + 1)/25 + 1 == currentPage {
+                self.fetchBeer(of: currentPage)
+            }
+        }
     }
 }
