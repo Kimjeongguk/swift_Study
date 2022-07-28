1e4350b8ae84cf617af22aa4e631b8b3179121bd
KimJeongguk <mindstar7949@naver.com>
2022-07-28 22:38:25 +0900

    netflixApp 수정

--- NetflixStyleSampleApp/NetflixStyleSampleApp/ContentDetailView.swift
+++ NetflixStyleSampleApp/NetflixStyleSampleApp/ContentDetailView.swift
@@ -10,18 +10,27 @@ import SwiftUI
 struct ContentDetailView: View {
     @State var item: Item?
     var body: some View {
-        VStack {
-            if let item = item {
-                Image(uiImage: item.image)
-                    .aspectRatio(contentMode: .fit)
-                
-                Text(item.description)
-                    .font(.caption)
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center)
-                    .padding()
-            } else {
-                Color.white
+        ZStack{
+            Color.black.edgesIgnoringSafeArea(.all)
+            ZStack(alignment: .bottom) {
+                if let item = item {
+                    Image(uiImage: item.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200)
+                    Text(item.description)
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .foregroundColor(.primary)
+                        .background(Color.primary
+                            .colorInvert()
+                            .opacity(0.75)
+                        )
+                } else {
+                    Color.white
+                }
             }
         }
     }
@@ -30,6 +39,10 @@ struct ContentDetailView: View {
 struct ContentDetailView_Previews: PreviewProvider {
     static var previews: some View {
         let item0 = Item(description: "흥미진진, 판타지, 애니메이션, 액션, 멀티캐슽팅", imageName: "poster0")
-        ContentDetailView(item: item0)
+        Group {
+            ContentDetailView(item: item0) 
+                .preferredColorScheme(.light)
+                .previewInterfaceOrientation(.portrait)
+        }
     }
 }
