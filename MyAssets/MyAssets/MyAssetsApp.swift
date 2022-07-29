16d6d80e09bc445e5ed6c8d2ab6f2d71ba28e06f
Kimjeongguk <mindstar7949@naver.com>
2022-07-29 17:31:09 +0900

    SwiftUI Grid & ScrollView

---
+++ MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by jeongguk on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
