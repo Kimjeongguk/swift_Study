dd677757b75cd49a9d0e4010ec57073a20eaef51
jeongguk <star7949@thinkm.co.kr>
2022-08-10 17:53:24 +0900

    Instagram App create

---
+++ StudyInstargram/StudyInstargram/Scenes/FeedViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  StudyInstargram
+//
+//  Created by jeongguk on 2022/08/10.
+//
+
+import UIKit
+import SnapKit
+
+class FeedViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        
+        tableView.register(FeedTableViewCell.self, forCellReuseIdentifier: "FeedTableViewCell")
+        return tableView
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        setupTableView()
+    }
+
+
+}
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell else { return UITableViewCell() }
+        
+        cell.selectionStyle = .none
+        cell.setup()
+        return cell
+    }
+}
+
+private extension FeedViewController {
+    func setupNavigationBar() {
+        navigationItem.title = "StudyInstargram"
+        
+        let uploadButton = UIBarButtonItem(
+            image: UIImage(systemName: "plus.app"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        navigationItem.rightBarButtonItem = uploadButton
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
---
+++ StudyInstargram/StudyInstargram/UIButton+.swift
@@ -0,0 +1,20 @@
+//
+//  UIButton+.swift
+//  StudyInstargram
+//
+//  Created by jeongguk on 2022/08/10.
+//
+
+import UIKit
+
+extension UIButton {
+    func setImage(systemName: String) {
+        contentHorizontalAlignment = .fill
+        contentVerticalAlignment = .fill
+        
+        imageView?.contentMode = .scaleAspectFit
+        imageEdgeInsets = .zero
+        
+        setImage(UIImage(systemName: systemName), for: .normal)
+    }
+}
