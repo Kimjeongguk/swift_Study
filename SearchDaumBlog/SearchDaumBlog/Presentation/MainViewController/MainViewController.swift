b9054c3f23937409920d4554c4a7eb7d77c5a09f
jeongguk <star7949@thinkm.co.kr>
2023-08-21 17:41:39 +0900

    rxswift 적용

--- SearchDaumBlog/SearchDaumBlog/Presentation/MainViewController/MainViewController.swift
+++ SearchDaumBlog/SearchDaumBlog/Presentation/MainViewController/MainViewController.swift
@@ -16,7 +16,7 @@ class MainViewController: UIViewController {
     let searchBar = SearchBar()
     let listView = BloagListView()
     
-    
+    let alertActionTapped = PublishRelay<AlertAction>()
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -30,8 +30,94 @@ class MainViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     private func bind() {
+        let blogResult = searchBar.shouldLoadResult
+            .flatMapLatest { query in
+                SearchBlogNetwork().searchBlog(query: query)
+            }
+            .share()
         
+        let blogValue = blogResult
+            .compactMap { data -> DKBlog? in
+                guard case .success(let value) = data else {
+                    return nil
+                }
+                return value
+            }
         
+        let blogError = blogResult
+            .compactMap { data -> String? in
+                guard case .failure(let error) = data else {
+                    return nil
+                }
+                return error.localizedDescription
+            }
+        
+        let cellData = blogValue
+            .map { blog -> [BlogListCellData] in
+                return blog.documents
+                    .map { doc in
+                        let thumbnailURL = URL(string: doc.thumbnail ?? "")
+                        return BlogListCellData(
+                            thumnailURL: thumbnailURL, name: doc.name, title: doc.title, datetime: doc.datetime
+                        )
+                    }
+            }
+        
+        let sortedType = alertActionTapped
+            .filter {
+                switch $0 {
+                case .title, .datetime:
+                    return true
+                default:
+                    return false
+                }
+            }
+            .startWith(.title)
+        
+        Observable
+            .combineLatest(
+                sortedType,
+                cellData
+            ) { type, data -> [BlogListCellData] in
+                switch type {
+                case .title:
+                    return data.sorted { $0.title ?? "" < $1.title ?? "" }
+                case .datetime:
+                    return data.sorted { $0.datetime ?? Date() > $1.datetime ?? Date() }
+                default:
+                    return data
+                }
+            }
+            .bind(to: listView.cellData)
+            .disposed(by: disposeBag)
+        
+        let alertSheetForSorting = listView.headerView.sortButtonTapped
+            .map { _ -> Alert in
+                return (title: nil, message: nil, actions: [.title, .datetime, .cancel], style: .actionSheet)
+            }
+        
+        let alertForErrorMessage = blogError
+            .map { message -> Alert in
+                return (
+                    title: "앗!",
+                    message: "예상치 못한 오류가 발생했습니다. 잠시 후 다시 시도해주세요. \(message)",
+                    actions: [.confirm],
+                    style: .alert
+                )
+                
+            }
+        Observable
+            .merge(
+                alertSheetForSorting,
+                alertForErrorMessage
+            )
+            .asSignal(onErrorSignalWith: .empty())
+            .flatMapLatest { alert -> Signal<AlertAction> in
+                let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: alert.style)
+                return self.presentAlertController(alertController, actions: alert.actions)
+            }
+            .emit(to: alertActionTapped)
+            .disposed(by: disposeBag)
     }
     
     private func attribute() {
@@ -51,5 +137,60 @@ class MainViewController: UIViewController {
             make.top.equalTo(searchBar.snp.bottom)
             make.leading.trailing.bottom.equalToSuperview()
         }
+    }
+}
+
+//Alert
+
+extension MainViewController {
+    typealias Alert = (title: String?, message: String?, actions: [AlertAction], style: UIAlertController.Style)
+    
+    enum AlertAction: AlertActionConvertible {
+        case title, datetime, cancel
+        case confirm
+        
+        var title: String {
+            switch self {
+            case.title:
+                return "Title"
+            case.datetime:
+                return "Datetime"
+            case .cancel:
+                return "취소"
+            case .confirm:
+                return "확인"
+            }
+        }
+        
+        var style: UIAlertAction.Style {
+            switch self {
+            case .title, .datetime:
+                return .default
+            case .cancel, .confirm:
+                return .cancel
+            }
+        }
+    }
+    
+    func presentAlertController<Action: AlertActionConvertible>(_ alertController: UIAlertController, actions: [Action]) -> Signal<Action> {
+        if actions.isEmpty { return .empty() }
+        return Observable
+            .create {[weak self] observer in
+                guard let self = self else { return Disposables.create() }
+                for action in actions {
+                    alertController.addAction(
+                        UIAlertAction(title: action.title, style: action.style, handler: { _ in
+                            observer.onNext(action)
+                            observer.onCompleted()
+                        })
+                    )
+                }
+                self.present(alertController, animated: true)
+                
+                return Disposables.create {
+                    alertController.dismiss(animated: true)
+                }
+            }
+            .asSignal(onErrorSignalWith: .empty())
     }
 }
